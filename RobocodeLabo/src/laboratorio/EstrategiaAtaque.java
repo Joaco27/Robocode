1f30218d1178310c3610b7f3a6cd80ec89fa7ad8
Manuel-Rubiano <manurubiano@hotmail.com>
2024-09-02 19:02:03 -0300

    ultimos detalles

--- RobocodeLabo/src/laboratorio/EstrategiaAtaque.java
+++ RobocodeLabo/src/laboratorio/EstrategiaAtaque.java
@@ -5,7 +5,9 @@ public abstract class EstrategiaAtaque {
 	public abstract void onScannedRobot(LaboRobot robot);
 	
 	public void onHitByBullet(LaboRobot robot) {
+		robot.turnGunTo(robot.scannedAngle);
 		robot.ahead(100);
+		robot.turnGunTo(robot.scannedAngle);
 		
 		
 	}
--- RobocodeLabo/src/laboratorio/EstrategiaMovimiento.java
+++ RobocodeLabo/src/laboratorio/EstrategiaMovimiento.java
@@ -5,12 +5,8 @@ public abstract class EstrategiaMovimiento {
 	public abstract void run(LaboRobot robot);
 	
 	public void onHitWall(LaboRobot robot) {
-		//robot.back(20);
-		
-		//int giro = (robot.hitWallAngle + 180) % 360;
-		
+		robot.turnGunTo(robot.scannedAngle);
 		int giro = 180;
-		//if (giro<0) giro+=360;
 		robot.turnRight(giro);
 	}
 
